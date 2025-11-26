5e3d87fb0a6df365fcb90f65c9d741a5179c49b8
Sebastián Galiano <sgaliano@gmail.com>
2025-11-26 16:04:09 -0300

    Clean up and simplify `hardhat.config.ts`

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,61 +1,39 @@
-import type { HardhatUserConfig, HttpNetworkUserConfig } from "hardhat/types";
-import "@nomiclabs/hardhat-etherscan";
+import type { HardhatUserConfig } from "hardhat/types";
+
+import "dotenv/config";
 import "@nomiclabs/hardhat-waffle";
 import "solidity-coverage";
 import "hardhat-deploy";
-import dotenv from "dotenv";
-import yargs from "yargs";
-import { getSingletonFactoryInfo } from "@safe-fndn/safe-singleton-factory";
 
-const argv = yargs
-  .option("network", {
-    type: "string",
-    default: "hardhat",
-  })
-  .help(false)
-  .version(false).argv;
+import "./src/tasks/local_verify";
+import "./src/tasks/deploy_contracts";
+import "./src/tasks/show_codesize";
 
-// Load environment variables.
-dotenv.config();
-const { NODE_URL, INFURA_KEY, MNEMONIC, ETHERSCAN_API_KEY, PK, SOLIDITY_VERSION, SOLIDITY_SETTINGS, CUSTOM_DETERMINISTIC_DEPLOYMENT } = process.env;
-
-const DEFAULT_MNEMONIC =
-  "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
-
-const sharedNetworkConfig: HttpNetworkUserConfig = {};
-if (PK) {
-  sharedNetworkConfig.accounts = [PK];
-} else {
-  sharedNetworkConfig.accounts = {
-    mnemonic: MNEMONIC || DEFAULT_MNEMONIC,
-  };
-}
-
-if (["mainnet", "rinkeby", "kovan", "goerli"].includes(argv.network) && INFURA_KEY === undefined) {
-  throw new Error(
-    `Could not find Infura key in env, unable to connect to network ${argv.network}`,
-  );
-}
-
-import "./src/tasks/local_verify"
-import "./src/tasks/deploy_contracts"
-import "./src/tasks/show_codesize"
 import { BigNumber } from "@ethersproject/bignumber";
+import { getSingletonFactoryInfo } from "@safe-fndn/safe-singleton-factory";
+import env from "env-var";
 
-const primarySolidityVersion = SOLIDITY_VERSION || "0.7.6"
-const soliditySettings = !!SOLIDITY_SETTINGS ? JSON.parse(SOLIDITY_SETTINGS) : undefined
+// Deployer account
+const MNEMONIC_PHRASE = env.get("MNEMONIC").asString();
+const PRIVATE_KEY = env.get("PK").required(!MNEMONIC_PHRASE).asString();
 
-const deterministicDeployment = CUSTOM_DETERMINISTIC_DEPLOYMENT == "true" ?
-  (network: string) => {
-    const info = getSingletonFactoryInfo(parseInt(network))
-    if (!info) return undefined
-    return {
-      factory: info.address,
-      deployer: info.signerAddress,
-      funding: BigNumber.from(info.gasLimit).mul(BigNumber.from(info.gasPrice)).toString(),
-      signedTx: info.transaction
-    }
-  } : undefined
+// Custom network
+const RPC_NODE_URL: string = env.get("NODE_URL").required().asUrlString();
+
+// Custom deployment
+const REPLAY_PROTECTION = env.get("CUSTOM_DETERMINISTIC_DEPLOYMENT").asBool();
+
+// Compiler configuration
+const SOLIDITY_VERSION = env.get("SOLIDITY_VERSION").default("0.7.6").asString();
+const SOLIDITY_SETTINGS = env.get("SOLIDITY_SETTINGS").asJson();
+
+// Contract Verification API
+const ETHERSCAN_API_URL = env.get("ETHERSCAN_API_URL").required().asString();
+const ETHERSCAN_API_KEY = env.get("ETHERSCAN_API_KEY").default("").asString();
+
+if (!PRIVATE_KEY && !MNEMONIC_PHRASE) {
+  throw new Error("Please set a private key or a mnemonic phrase");
+}
 
 const userConfig: HardhatUserConfig = {
   paths: {
@@ -66,77 +44,52 @@ const userConfig: HardhatUserConfig = {
   },
   solidity: {
     compilers: [
-      { version: primarySolidityVersion, settings: soliditySettings },
+      { version: SOLIDITY_VERSION, settings: SOLIDITY_SETTINGS },
       { version: "0.6.12" },
       { version: "0.5.17" },
-    ]
+    ],
   },
   networks: {
     hardhat: {
       allowUnlimitedContractSize: true,
       blockGasLimit: 100000000,
-      gas: 100000000
+      gas: 100000000,
     },
-    mainnet: {
-      ...sharedNetworkConfig,
-      url: `https://mainnet.infura.io/v3/${INFURA_KEY}`,
+    custom: {
+      url: RPC_NODE_URL,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : { mnemonic: `${MNEMONIC_PHRASE}` },
+      verify: {
+        etherscan: {
+          apiUrl: ETHERSCAN_API_URL,
+          apiKey: ETHERSCAN_API_KEY,
+        },
+      },
     },
-    xdai: {
-      ...sharedNetworkConfig,
-      url: "https://xdai.poanetwork.dev",
-    },
-    ewc: {
-      ...sharedNetworkConfig,
-      url: `https://rpc.energyweb.org`,
-    },
-    rinkeby: {
-      ...sharedNetworkConfig,
-      url: `https://rinkeby.infura.io/v3/${INFURA_KEY}`,
-    },
-    goerli: {
-      ...sharedNetworkConfig,
-      url: `https://goerli.infura.io/v3/${INFURA_KEY}`,
-    },
-    kovan: {
-      ...sharedNetworkConfig,
-      url: `https://kovan.infura.io/v3/${INFURA_KEY}`,
-    },
-    polygon: {
-      ...sharedNetworkConfig,
-      url: `https://polygon-mainnet.infura.io/v3/${INFURA_KEY}`,
-    },
-    volta: {
-      ...sharedNetworkConfig,
-      url: `https://volta-rpc.energyweb.org`,
-    },
-    bsc: {
-      ...sharedNetworkConfig,
-      url: `https://bsc-dataseed.binance.org/`,
-    },
-    arbitrum: {
-      ...sharedNetworkConfig,
-      url: `https://arb1.arbitrum.io/rpc`,
-    },
-    fantomTestnet: {
-      ...sharedNetworkConfig,
-      url: `https://rpc.testnet.fantom.network/`,
-    }
   },
-  deterministicDeployment,
   namedAccounts: {
     deployer: 0,
   },
   mocha: {
     timeout: 2000000,
   },
-  etherscan: {
-    apiKey: ETHERSCAN_API_KEY,
-  },
 };
-if (NODE_URL) {
-  userConfig.networks!!.custom = {
-    ...sharedNetworkConfig,
-    url: NODE_URL,
-  }
+
+if (REPLAY_PROTECTION) {
+  userConfig.deterministicDeployment = (network: string) => {
+    const chainId = parseInt(network);
+    const info = getSingletonFactoryInfo(chainId);
+
+    if (!info) return undefined;
+
+    const funding = BigNumber.from(info.gasLimit).mul(BigNumber.from(info.gasPrice)).toString();
+
+    return {
+      factory: info.address,
+      deployer: info.signerAddress,
+      signedTx: info.transaction,
+      funding,
+    };
+  };
 }
-export default userConfig
+
+export default userConfig;
